8a5c18f47df261318280435d98745dddd933bc29
Ángel Vázquez <angel_betta@outlook.com>
2019-09-23 22:34:45 -0500

    DAY-001: Add playground for simple types

---
+++ Introduction/Day01/Day01.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+// VARIABLES
+var str = "Hello, playground"
+str = "Goodbye"
+
+// STRINGS AND INTEGERS
+var age = 38
+var population = 8_000_000
+
+// MULTI-LINE STRINGS
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// If you don't want those line breaks to actually be in the string.
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// DOUBLES AND BOOLEANS
+var pi = 3.141
+var awesome = true
+
+// STRING INTERPOLATION
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+// CONSTANTS
+let taylor = "swift"
+
+// TYPE ANNOTATIONS
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
