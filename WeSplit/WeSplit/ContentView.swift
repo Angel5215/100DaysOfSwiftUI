a2a72259639956cea4834b9802574db291951ff6
Ángel Vázquez <angel_betta@outlook.com>
2019-10-08 23:00:24 -0500

    DAY-016: Add a NavigationView with a small title

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello World")
-                Text("Hello World")
-            }
-            
-            Section {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
-            }
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+                
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+            }.navigationBarTitle("SwiftUI", displayMode: .inline)
         }
     }
 }
