6eb6b5eaf5717bea37ba68edbbd0ecacaea1db01
Ángel Vázquez <angel_betta@outlook.com>
2019-10-08 23:38:24 -0500

    DAY-016: Revert changes to initial app state

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,16 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
-    
     var body: some View {
-        Picker("Select your student", selection: $selectedStudent) {
-            ForEach(0 ..< students.count) {
-                Text(self.students[$0])
-            }
-        }
+        Text("Hello world")
     }
 }
 
