7d3023971f3404a4a2e808cf5b7261ef7ce129f1
Ángel Vázquez <angel_betta@outlook.com>
2019-10-08 22:22:58 -0500

    DAY-016: Add initial files for project one

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Angel Vázquez on 08/10/19.
+//  Copyright © 2019 Ángel Vázquez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
