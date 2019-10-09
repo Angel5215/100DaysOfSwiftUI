2dc616d007e83ebb99324449e155fe317cefe310
Ángel Vázquez <angel_betta@outlook.com>
2019-10-08 23:27:41 -0500

    DAY-016: Use the dollar sign to create a two-way binding for a property inside a TextField

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var tapCount = 0
+    @State private var name = ""
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            self.tapCount += 1
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Your name is \(name)")
         }
     }
 }
