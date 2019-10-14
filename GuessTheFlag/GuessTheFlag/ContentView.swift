cdee87d58cd0fc9e23a5c7d26371e11fafa58ce6
Ángel Vázquez <angel_betta@outlook.com>
2019-10-13 21:10:10 -0500

    DAY-021: Add basic UI with three tappable flags

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,14 +12,31 @@ struct ContentView: View {
     
     @State private var showingAlert = false
     
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Button("Show alert") {
-            self.showingAlert = true
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SwiftUI!"),
-                  message: Text("This is some detail message"),
-                  dismissButton: .default(Text("OK")))
+        ZStack {
+            
+            Color.blue.edgesIgnoringSafeArea(.all)
+                    
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    })
+                }
+                
+                Spacer()
+            }
         }
     }
 }
