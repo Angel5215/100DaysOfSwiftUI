7e3495215d266aed007692a292d48be14f6149c1
Ángel Vázquez <angel_betta@outlook.com>
2019-10-13 23:00:53 -0500

    DAY-021: Add cosmetic improvements to overall UI

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -18,12 +18,15 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
                     
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                     Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                 }
                 
                 ForEach(0 ..< 3) { number in
@@ -32,9 +35,11 @@ struct ContentView: View {
                     }, label: {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
                     })
                 }
-                
                 Spacer()
             }
         }
