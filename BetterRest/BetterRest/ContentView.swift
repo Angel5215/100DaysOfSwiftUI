ab34ffc1482dd3ff78e16384ca0d4dac31d20647
Ángel Vázquez <angel_betta@outlook.com>
2019-10-19 18:09:35 -0500

    DAY-026: Create a date picker without its label that shows hours and minutes

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,10 +11,20 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
     
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%g") hours")
+        VStack {
+            Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                Text("\(sleepAmount, specifier: "%g") hours")
+            }
+            
+            
+            DatePicker("Please enter a date",
+                       selection: $wakeUp,
+                       in: Date()...,
+                       displayedComponents: .hourAndMinute)
+                .labelsHidden()
         }
     }
 }
