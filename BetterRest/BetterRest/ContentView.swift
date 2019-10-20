18b2ef6c0f7ec7e0ebcdc8f58e36c337b5e5c662
Ángel Vázquez <angel_betta@outlook.com>
2019-10-19 20:43:45 -0500

    DAY-027: Create the basic UI for the app

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,22 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                Text("\(sleepAmount, specifier: "%g") hours")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }
             }
-            
-            
-            DatePicker("Please enter a date",
-                       selection: $wakeUp,
-                       in: Date()...,
-                       displayedComponents: .hourAndMinute)
-                .labelsHidden()
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing: Button(action: calculateBedtime) {
+                Text("Calculate")
+            })
         }
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
