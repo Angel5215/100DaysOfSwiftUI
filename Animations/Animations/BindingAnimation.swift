be35bb9a9db6190a0f5962bd8ba37f0c00ffe753
Ángel Vázquez <angel_betta@outlook.com>
2019-10-24 22:11:02 -0500

    DAY-032: Add Navigation links to the animations created

---
+++ Animations/Animations/BindingAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  BindingAnimation.swift
+//  Animations
+//
+//  Created by Angel Vázquez on 24/10/19.
+//  Copyright © 2019 Ángel Vázquez. All rights reserved.
+//
+
+import SwiftUI
+
+struct BindingAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingAnimation()
+    }
+}
--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,29 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var animationAmount: CGFloat = 1.0
-    
     var body: some View {
-        Button("Tap me") {
-            //self.animationAmount += 1
-        }
-        .padding(40)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(Color.red)
-                .scaleEffect(animationAmount)
-                .opacity(Double(2 - animationAmount))
-                .animation(
-                    Animation.easeOut(duration: 1)
-                        .repeatForever(autoreverses: false)
-                )
-        )
-        .onAppear {
-            self.animationAmount = 2
+        NavigationView {
+            List {
+                NavigationLink("Pulsating animation", destination: PulsatingAnimation())
+                    .labelsHidden()
+                NavigationLink("Animating bindings", destination: BindingAnimation())
+            }
+            .navigationBarTitle("Animations")
         }
     }
 }
