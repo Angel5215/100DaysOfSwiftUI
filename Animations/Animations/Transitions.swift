ba40b779e0ac30522fb61b75112c0af283a56210
Ángel Vázquez <angel_betta@outlook.com>
2019-10-26 17:29:23 -0500

    DAY-033: Create a custom transition that looks like Keynote pivot

--- Animations/Animations/Transitions.swift
+++ Animations/Animations/Transitions.swift
@@ -24,9 +24,27 @@ struct Transitions: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
             }
         }
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
     }
 }
 
