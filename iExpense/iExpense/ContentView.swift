22263c2c29e2fdf4b40ca6a293a6b44209a28d39
Ángel Vázquez <angel_betta@outlook.com>
2019-10-29 21:56:08 -0600

    DAY-037: Add a list of expenses with a delete function

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,77 +8,36 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
 struct ContentView: View {
     
-    @ObservedObject var user = User()
-    @State private var showingSheet = false
-    
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @ObservedObject var expenses = Expenses()
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Your name is \(user.firstName) \(user.lastName)")
-                
-                TextField("First name", text: $user.firstName)
-                TextField("Last name", text: $user.lastName)
-                Text("Tap count: \(tapCount)")
-                
-                Button("Show sheet") {
-                    self.showingSheet.toggle()
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
                 }
-                
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("\($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                Button("Add number") {
-                    self.numbers.append(self.currentNumber)
-                    self.currentNumber += 1
-                    self.tapCount += 1
-                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
-                }
-                
+                .onDelete(perform: removeItems)
             }
-            .sheet(isPresented: $showingSheet) {
-                SecondView(name: self.user.firstName)
-            }
-            .navigationBarItems(leading: EditButton())
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            )
         }
     }
     
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
+    // MARK: - Methods
+    private func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
-struct SecondView: View {
-    
-    @Environment(\.presentationMode) var presentationMode
-    var name: String
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            Text("Hello, \(name)")
-            Spacer()
-            Button("Dismiss") {
-                self.presentationMode.wrappedValue.dismiss()
-            }
-            Spacer()
-        }
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,19 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Angel Vázquez on 29/10/19.
+//  Copyright © 2019 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
