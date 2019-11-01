4694cdaee3e8175075c87254af414eb0d2855880
Ángel Vázquez <angel_betta@outlook.com>
2019-10-31 21:11:01 -0600

    DAY-039: Add a simple scroll view with 100 texts

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("high-sierra")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: geo.size.width)
+        ScrollView(.vertical) {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
+    }
+}
+
+struct CustomText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
 
