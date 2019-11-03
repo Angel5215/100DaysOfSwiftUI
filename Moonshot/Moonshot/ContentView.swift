fb2e72246d82bfaeafb9e864515c62bc4209b066
Ángel Vázquez <angel_betta@outlook.com>
2019-11-02 19:41:03 -0600

    DAY-040: Create the main user interface

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -14,7 +14,25 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView {
+            List {
+                ForEach(missions) { mission in
+                    NavigationLink(destination: Text("Detail view")) {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 44, height: 44)
+                        
+                        VStack(alignment: .leading) {
+                            Text(mission.displayName)
+                                .font(.headline)
+                            Text(mission.formattedLaunchDate)
+                        }
+                    }
+                }
+                .navigationBarTitle("Moonshot")
+            }
+        }
     }
 }
 
--- Moonshot/Moonshot/JSON/Mission.swift
+++ Moonshot/Moonshot/JSON/Mission.swift
@@ -10,13 +10,34 @@ import Foundation
 
 struct Mission: Codable, Identifiable {
     
+    // MARK: - Nested struct
     struct CrewRole: Codable {
         let name: String
         let role: String
     }
     
+    // MARK: - Properties
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
+    
+    // MARK: - Computed properties
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
 }
