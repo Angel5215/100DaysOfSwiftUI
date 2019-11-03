397f87ed0da2891e3344c67a1422553ab41095f6
Ángel Vázquez <angel_betta@outlook.com>
2019-11-02 18:21:50 -0600

    DAY-040: Decode astronauts JSON into an array to use

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Angel Vázquez on 02/11/19.
+//  Copyright © 2019 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,47 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink(destination: DecodableView()) {
-                    Text("Row \(row)")
-                }
-            }
-            .navigationBarTitle("SwiftUI")
-        }
-    }
-}
-
-struct User: Codable {
-    var name: String
-    var address: Address
-}
-
-struct Address: Codable {
-    var street: String
-    var city: String
-}
-
-struct DecodableView: View {
-    var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
-                }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
-            }
-        }
+        Text("\(astronauts.count)")
     }
 }
 
