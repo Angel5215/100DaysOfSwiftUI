e55c2b8d1d002a59bb87aec3ca481230466afaba
Ángel Vázquez <angel_betta@outlook.com>
2019-11-02 20:16:12 -0600

    DAY-041: Merge data from two JSON files to show inside the detail view screen

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(missions) { mission in
-                    NavigationLink(destination: Text("Detail view")) {
+                    NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
                         Image(mission.image)
                             .resizable()
                             .scaledToFit()
