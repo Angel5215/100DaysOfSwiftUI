c1086e229ff58f1e8a6fb8ab036cd86797d7d4e2
Ángel Vázquez <angel_betta@outlook.com>
2019-11-04 21:58:24 -0600

    DAY-043: Create a triangle and arc shapes

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -10,14 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
+        NavigationView {
+            ScrollView(.vertical) {
+                VStack {
+                    Triangle()
+                        .fill(Color.red)
+                        .frame(width: 300, height: 300)
+                    
+                    Spacer(minLength: 50)
+                    
+                    Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+                        .stroke(Color.blue, lineWidth: 10)
+                        .frame(width: 300, height: 300)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .navigationBarTitle("Drawing")
         }
-        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: Shape {
+    
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                    radius: rect.width / 2,
+                    startAngle: modifiedStart,
+                    endAngle: modifiedEnd,
+                    clockwise: !clockwise)
+        
+        return path
     }
 }
 
