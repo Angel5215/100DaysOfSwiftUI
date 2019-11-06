7c899d37213328e95b08c1fc0925ed3468c14868
Ángel Vázquez <angel_betta@outlook.com>
2019-11-05 21:13:23 -0600

    DAY-044: Transform a flower-like shape using CGAffineTransforms and even-odd fills

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -9,9 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Arc(startAngle: .degrees(0), endAngle: .degrees(120), clockwise: true)
-            .strokeBorder(Color.blue, lineWidth: 40)
+        
+        print(petalOffset, petalWidth)
+        return VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40).padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100).padding([.horizontal, .bottom])
+        }
     }
 }
 
@@ -54,6 +68,39 @@ struct Arc: InsettableShape {
         var arc = self
         arc.insetAmount += amount
         return arc
+    }
+}
+
+struct Flower: Shape {
+    
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+    
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // Count from 0 up to 2π, moving up π/8 each time
+        for number in stride(from: 0, through: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // Rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // Move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // Create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            
+            // apply our rotation/position transform to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to the main path
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
     }
 }
 
--- Drawing/README.md
+++ Drawing/README.md
@@ -15,3 +15,14 @@ This technique project takes a close look at drawing in SwiftUI, including creat
 
 - **`Path`** is a dedicated type given to us by SwiftUI used for drawing custom shapes. It's very low level so you will usually want to wrap it in something else to make it more usefult but it is still the building block that underlies other work.
 
+- Like colors, gradients and shapes, paths are also a `View`.
+
+- SwiftUI’s dedicated `ShapeStyle` struct gives us control over how every line should be connected to the line after it (**line join**) and how every line should be drawn when it ends without a connection after it (**line cap**)
+
+- SwiftUI enables custom drawing with two subtly different types: **paths and shapes**. 
+    - A path is a series of drawing instructions such as “start here, draw a line to here, then add a circle there”, all using absolute coordinates. 
+    - A shape has no idea where it will be used or how big it will be used, but instead will be asked to draw itself inside a given rectangle.
+
+- Shapes are also views.
+
+- SwiftUI 0 degrees is not straight upwards, but instead directly to the right. Shapes measure their coordinates from the bottom-left corner rather than the top-left corner, which means SwiftUI goes the other way around from one angle to the other.
