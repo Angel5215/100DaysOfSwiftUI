54eaba052c0ce9d692ac5bc8243331046bb11125
Ángel Vázquez <angel_betta@outlook.com>
2019-11-05 23:18:19 -0600

    DAY-044: Fix performance problems when drawing 100 gradients with the drawingGroup() modifier

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -12,11 +12,44 @@ struct ContentView: View {
     
     @State private var petalOffset = -20.0
     @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
+        }
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
     
     var body: some View {
-        Capsule()
-            .strokeBorder(ImagePaint(image: Image("images"), scale: 0.1), lineWidth: 20)
-            .frame(width: 300, height: 200)
+        ZStack {
+            ForEach(0 ..< steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
