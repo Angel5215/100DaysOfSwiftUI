d70735d18418cf6e2a0ca8b455a0bfefaef2760c
Ángel Vázquez <angel_betta@outlook.com>
2019-11-06 23:05:07 -0600

    DAY-045: Animate changes in a trapezoid Shape using animatable data

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -52,10 +52,52 @@ struct ContentView: View {
                         Image(systemName: "8.circle")
                         Text("Saturation and blur")
                     }
+                    
+                    NavigationLink(destination: TrapezoidView()) {
+                        Image(systemName: "9.circle")
+                        Text("Animating shapes")
+                    }
                 }
             }
             .navigationBarTitle("Drawing")
         }
+    }
+}
+
+struct TrapezoidView: View {
+    
+    @State private var insetAmount: CGFloat = 50
+    
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+            }
+    }
+}
+
+struct Trapezoid: Shape {
+    
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
     }
 }
 
