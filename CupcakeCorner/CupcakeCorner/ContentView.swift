01dc2598499471750140ef519bf49f723f535f16
Ángel Vázquez <angel_betta@outlook.com>
2019-11-10 22:34:24 -0600

    DAY-049: Download data using URLSession and disable fields in a form

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -26,9 +26,70 @@ class User: ObservableObject, Codable {
     }
 }
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    
+    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }
+            .onAppear(perform: loadData)
+            
+            
+            Form {
+                Section {
+                    TextField("Username", text: $username)
+                    TextField("Email", text: $email)
+                }
+                
+                Section {
+                    Button("Create account") {
+                        print("Creating account")
+                    }
+                }
+                .disabled(username.isEmpty || email.isEmpty)
+
+            }
+        }
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
