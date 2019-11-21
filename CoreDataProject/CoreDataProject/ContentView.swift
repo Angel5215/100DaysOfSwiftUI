b37131e5fd6b36c02956d6dda7f5ffc012b95bc2
Ángel Vázquez <angel_betta@outlook.com>
2019-11-21 00:13:09 -0600

    DAY-058: Filtering fetch request using NSPredicates

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -13,7 +13,56 @@ struct Student: Hashable {
 }
 
 struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink("Core Data constraints", destination: UniqueConstraintsView())
+                
+                NavigationLink("Filtering with NSPredicate", destination: FilteringView())
+            }
+            .navigationBarTitle("Core Data")
+        }
+    }
+}
+
+struct FilteringView: View {
     
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Ship.entity(),
+                  sortDescriptors: [],
+                  predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+    
+    var body: some View {
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? self.moc.save()
+            }
+        }
+        .navigationBarTitle("Filtering with NSPredicate", displayMode: .inline)
+    }
+}
+
+struct UniqueConstraintsView: View {
     let students = [
         Student(name: "Harry Potter"),
         Student(name: "Hermione Granger")
@@ -41,6 +90,7 @@ struct ContentView: View {
                 }
             }
         }
+        .navigationBarTitle("Core Data constraints", displayMode: .inline)
     }
 }
 
