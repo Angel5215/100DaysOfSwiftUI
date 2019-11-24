4f0a7b842e766336512215d061f64c6927ae00ae
Ángel Vázquez <angel_betta@outlook.com>
2019-11-23 23:29:34 -0600

    DAY-062: Display an action sheet to change a text background color

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -10,30 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var blurAmount: CGFloat = 0 {
-        didSet {
-            print("The new value is \(blurAmount)")
-        }
-    }
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.primary
     
     var body: some View {
-        
-        let blur = Binding<CGFloat>(
-            get: {
-                self.blurAmount
-            },
-            set: {
-                self.blurAmount = $0
-                print("New value is \(self.blurAmount)")
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
             }
-        )
-        
-        return VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-
-            Slider(value: blur, in: 0...20)
-        }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change background"),
+                            message: Text("Select a new color"),
+                            buttons: [
+                                .default(Text("Red")) { self.backgroundColor = .red },
+                                .default(Text("Green")) { self.backgroundColor = .green },
+                                .default(Text("Blue")) { self.backgroundColor = .blue },
+                                .cancel()
+                            ])
+            }
     }
 }
 
