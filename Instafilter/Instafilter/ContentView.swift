cc373d2eef5d7bb4e1ee12fa0395172888a5f278
Ángel Vázquez <angel_betta@outlook.com>
2019-11-23 23:10:21 -0600

    DAY-062: Create a custom binding to trigger a property observer when a @State property changes

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -17,11 +17,22 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
+        
+        let blur = Binding<CGFloat>(
+            get: {
+                self.blurAmount
+            },
+            set: {
+                self.blurAmount = $0
+                print("New value is \(self.blurAmount)")
+            }
+        )
+        
+        return VStack {
             Text("Hello, World!")
                 .blur(radius: blurAmount)
 
-            Slider(value: $blurAmount, in: 0...20)
+            Slider(value: blur, in: 0...20)
         }
     }
 }
