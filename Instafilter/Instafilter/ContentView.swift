9acff2b5b4c44d65404a286af9b6bce74fdc502c
Ángel Vázquez <angel_betta@outlook.com>
2019-11-24 20:35:48 -0600

    DAY-063: Apply a Twirl Distortion filter to an image and integrate CoreImage with SwiftUI

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -6,30 +6,38 @@
 //  Copyright © 2019 Ángel Vázquez. All rights reserved.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingActionSheet = false
-    @State private var backgroundColor = Color.primary
+    @State private var image: Image?
     
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                self.showingActionSheet = true
-            }
-            .actionSheet(isPresented: $showingActionSheet) {
-                ActionSheet(title: Text("Change background"),
-                            message: Text("Select a new color"),
-                            buttons: [
-                                .default(Text("Red")) { self.backgroundColor = .red },
-                                .default(Text("Green")) { self.backgroundColor = .green },
-                                .default(Text("Blue")) { self.backgroundColor = .blue },
-                                .cancel()
-                            ])
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "racoon") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
