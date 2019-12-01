9c2c38bfb27409bacd6f27b2ce392cd481570cb3
Ángel Vázquez <angel_betta@outlook.com>
2019-11-30 21:03:48 -0600

    DAY-069: Communicate with a MapKit coordinator

--- BucketList/BucketList/MapView.swift
+++ BucketList/BucketList/MapView.swift
@@ -11,13 +11,45 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     
+    // MARK: - UIViewRepresentable
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Capital of England"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+        mapView.addAnnotation(annotation)
+        
         return mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> MapView.Coordinator {
+        Coordinator(self)
+    }
+    
+    // MARK: - Coordinator
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
     }
 }
 
