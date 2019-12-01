dd7c00eb7f247ebabd8b874050409023c71f0612
Ángel Vázquez <angel_betta@outlook.com>
2019-11-30 19:50:15 -0600

    DAY-068: Make a custom type conform to Comparable

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -8,9 +8,27 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+
     var body: some View {
-        Text("Hello, World!")
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
+        }
     }
 }
 
