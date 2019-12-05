8c2c89876a2fe7f06f5e266c21665e6935f94759
Ángel Vázquez <angel_betta@outlook.com>
2019-12-04 22:40:24 -0600

    DAY-072: Add FaceID/TouchID authentication to show placemarks

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Ángel Vázquez. All rights reserved.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
@@ -16,42 +17,54 @@ struct ContentView: View {
     @State private var showingPlaceDetails = false
     @State private var locations = [CodableMKPointAnnotation]()
     @State private var showingEditScreen = false
+    @State private var isUnlocked = false
+    
     
     var body: some View {
         ZStack {
-            MapView(centerCoordinate: $centerCoordinate,
-                    selectedPlace: $selectedPlace,
-                    showingPlaceDetails: $showingPlaceDetails,
-                    annotations: locations)
-                .edgesIgnoringSafeArea(.all)
-            
-            Circle()
-                .fill(Color.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            
-            VStack {
-                Spacer()
-                HStack {
+            if isUnlocked {
+                MapView(centerCoordinate: $centerCoordinate,
+                        selectedPlace: $selectedPlace,
+                        showingPlaceDetails: $showingPlaceDetails,
+                        annotations: locations)
+                    .edgesIgnoringSafeArea(.all)
+                
+                Circle()
+                    .fill(Color.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                
+                VStack {
                     Spacer()
-                    
-                    Button(action: {
-                        let newLocation = CodableMKPointAnnotation()
-                        newLocation.coordinate = self.centerCoordinate
-                        newLocation.title = "Example location"
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
-                        self.showingEditScreen = true
-                    }, label: {
-                        Image(systemName: "plus")
-                    })
-                        .padding()
-                        .background(Color.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            let newLocation = CodableMKPointAnnotation()
+                            newLocation.coordinate = self.centerCoordinate
+                            newLocation.title = "Example location"
+                            self.locations.append(newLocation)
+                            self.selectedPlace = newLocation
+                            self.showingEditScreen = true
+                        }, label: {
+                            Image(systemName: "plus")
+                        })
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    }
                 }
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
             }
         }
         .alert(isPresented: $showingPlaceDetails) {
@@ -90,6 +103,28 @@ struct ContentView: View {
             try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
         } catch {
             print("Unable to save data.")
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate yourself to unlock your places."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        // error
+                    }
+                }
+            }
+        } else {
+            // no biometrics
         }
     }
 }
