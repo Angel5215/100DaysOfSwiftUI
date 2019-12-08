e034010bda605835a7500581e58620cd3efc1670
Ángel Vázquez <angel_betta@outlook.com>
2019-12-08 17:54:59 -0600

    DAY-075: Make each item inside the list be read as a single element

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -26,9 +26,13 @@ struct ContentView: View {
                     .padding()
                     .autocapitalization(.none)
                 
-                List(usedWords, id: \.self) {
-                    Image(systemName: "\($0.count).circle")
-                    Text($0)
+                List(usedWords, id: \.self) { word in
+                    HStack {
+                        Image(systemName: "\(word.count).circle")
+                        Text(word)
+                    }
+                    .accessibilityElement(children: .ignore)
+                    .accessibility(label: Text("\(word), \(word.count) letters"))
                 }
             }
             .navigationBarTitle(rootWord)
