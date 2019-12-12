28b9cedf0d4e1fd6aecef2084b9281cbad9d9513
Ángel Vázquez <angel_betta@outlook.com>
2019-12-12 00:48:41 -0600

    DAY-080: Using Swift's Result type with network data fetching

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -12,29 +12,54 @@ class User: ObservableObject {
     @Published var name = "Taylor Swift"
 }
 
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
+
 // MARK: - Views
 struct ContentView: View {
     
     @State private var selectedTab = 0
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .tabItem {
-                    Image(systemName: "star")
-                    Text("One")
+        Text("Hello, World!")
+            .onAppear {
+                self.fetchData(from: "https://www.apple.com/") { result in
+                    switch result {
+                    case .success(let str):
+                        print(str)
+                    case .failure(let error):
+                        switch error {
+                        case .badURL:
+                            print("Bad URL")
+                        case .requestFailed:
+                            print("Bad URL")
+                        case .unknown:
+                            print("Unknown error")
+                        }
+                    }
                 }
-                .onTapGesture {
-                    self.selectedTab = 1
-                }
-                .tag(0)
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-                }
-                .tag(1)
+            }
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
         }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    completion(.failure(.requestFailed))
+                } else {
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
     }
 }
 
