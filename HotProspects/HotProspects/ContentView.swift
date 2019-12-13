ab60159279596e25f760508985f16214b951e12c
Ángel Vázquez <angel_betta@outlook.com>
2019-12-12 22:39:04 -0600

    DAY-081: Add request permission and trigger a local notification

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 class User: ObservableObject {
     @Published var name = "Taylor Swift"
@@ -41,6 +42,31 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            
+            Button("Request permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                // triggers 5 seconds from now
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+            
             Text("Hello, World!")
                 .padding()
                 .background(backgroundColor)
--- HotProspects/README.md
+++ HotProspects/README.md
@@ -6,12 +6,13 @@ This is an app to track who you meet at conferences. It will show a QR code taht
 ## Topics
 
 - Environment (`@EnvironmentObject`)
-- Notifications
+- Local Notifications
 - Tab bars
 - Context menus
 - `Result`
 - `objectWillChange`
 - Image interpolation
+- Context menus
 
 ## Images
 
@@ -23,3 +24,7 @@ This is an app to track who you meet at conferences. It will show a QR code taht
 
 - Every class that conforms to `ObservableObject` gains a `objectWillChange` property. This is a **publisher**, which means it does the same job as the `@Published` property wrapper (announce changes to properties, so that any views using the object get their `body` property reinvoked). This publisher should be triggered **immediately before** we make our change to allow SwiftUI to examine the state of our UI and prepare for animation changes.
 
+- Local notifications are broken intor three parts to give them maximum flexibility:
+    - A **content** of what should be shown - this can be a title, subtitle, sound, image, and so on.
+    - A **trigger** that deterimnes when the notification should be shown - it can be from a few seconds from now to a date and time in the future, or even a location.
+    - A **request** which is the combination of content and trigger but also adds a **unique identifier** so you can edit or remove specific alerts later on.
