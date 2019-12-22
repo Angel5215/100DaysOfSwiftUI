41b2ca6d2f952b38b0c0d3beb227bb148de4ed90
Ángel Vázquez <angel_betta@outlook.com>
2019-12-22 15:52:19 -0600

    DAY-090: Make iPhones vibrate accordingly when swiping a card

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -16,6 +16,7 @@ struct CardView: View {
     
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    let feedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
@@ -59,9 +60,15 @@ struct CardView: View {
             DragGesture()
                 .onChanged { gesture in
                     self.offset = gesture.translation
+                    self.feedback.prepare()
                 }
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
                         self.removal?()
                     } else {
                         self.offset = .zero
