ca3bde087be59ffbfe70d0118b2474ead8c32841
Ángel Vázquez <angel_betta@outlook.com>
2019-12-21 23:31:54 -0600

    DAY-089: Count down 100 seconds with a timer and enable or disable it with a publisher

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -11,8 +11,12 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var timeRemaining = 100
+    @State private var isActive = true
+    
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
 
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -22,6 +26,18 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
+                
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -55,6 +71,18 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            self.isActive = true
         }
     }
     
