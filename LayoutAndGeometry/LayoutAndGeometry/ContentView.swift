9c3104ba0aa32f48652c88462b67bff51388060e
Ángel Vázquez <angel_betta@outlook.com>
2019-12-24 22:17:58 -0600

    DAY-093: Rotate views based on their global coordinates to make a DNA-like rotation effect

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,10 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        OuterView()
-            .background(Color.red)
-            .coordinateSpace(name: "Custom")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5),
+                                              axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
     }
 }
 
