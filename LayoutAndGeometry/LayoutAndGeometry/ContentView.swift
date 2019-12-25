3541db7772129f4ec139e041243c25fce253d28a
Ángel Vázquez <angel_betta@outlook.com>
2019-12-24 22:25:03 -0600

    DAY-093: ScrollView with CoverFlow like effect using GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -14,20 +14,22 @@ struct ContentView: View {
     
     var body: some View {
         GeometryReader { fullView in
-            ScrollView(.vertical) {
-                ForEach(0..<50) { index in
-                    GeometryReader { geo in
-                        Text("Row #\(index)")
-                            .font(.title)
-                            .frame(width: fullView.size.width)
-                            .background(self.colors[index % 7])
-                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5),
-                                              axis: (x: 0, y: 1, z: 0))
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(width: 150)
                     }
-                    .frame(height: 40)
                 }
+                .padding(.horizontal, ((fullView.size.width - 150) / 2))
             }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
