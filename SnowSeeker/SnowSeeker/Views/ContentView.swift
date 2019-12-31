577ea1068ee4aee57543e5e81b1cfba78d82c208
Ángel Vázquez <angel_betta@outlook.com>
2019-12-30 21:06:45 -0600

    DAY-099: Add a workaround to show ActionSheets in iPads and corrected Size labels in filter sheet

--- SnowSeeker/SnowSeeker/Views/ContentView.swift
+++ SnowSeeker/SnowSeeker/Views/ContentView.swift
@@ -24,8 +24,8 @@ struct ContentView: View {
         case countryLessM = "A - M"
         case countryMoreM = "M - Z"
         case sizeOne = "Small"
-        case sizeTwo = "Medium"
-        case sizeThree = "Big"
+        case sizeTwo = "Average"
+        case sizeThree = "Large"
         case priceOne = "$"
         case priceTwo = "$$"
         case priceThree = "$$$"
@@ -74,8 +74,8 @@ struct ContentView: View {
         Button("Sort") {
             self.showingSortSheet = true
         }
-        .actionSheet(isPresented: $showingSortSheet) {
-            ActionSheet(title: Text("Sort by..."), message: nil, buttons: [
+        .popSheet(isPresented: $showingSortSheet) {
+            PopSheet(title: Text("Sort by..."), message: nil, buttons: [
                 .default(Text(SortMethod.alphabetical.rawValue)) { self.sortMethod = .alphabetical },
                 .default(Text(SortMethod.country.rawValue)) { self.sortMethod = .country },
                 .default(Text(SortMethod.defaultSort.rawValue)) { self.sortMethod = .defaultSort },
@@ -88,8 +88,8 @@ struct ContentView: View {
         Button("Filter") {
             self.showingFilterSheet = true
         }
-        .actionSheet(isPresented: $showingFilterSheet) {
-            ActionSheet(title: Text("Filter by..."), message: nil, buttons: [
+        .popSheet(isPresented: $showingFilterSheet) {
+            PopSheet(title: Text("Filter by..."), message: nil, buttons: [
                 .default(Text(FilterMethod.countryLessM.rawValue)) { self.filterMethod = .countryLessM },
                 .default(Text(FilterMethod.countryMoreM.rawValue)) { self.filterMethod = .countryMoreM },
                 .default(Text(FilterMethod.sizeOne.rawValue)) { self.filterMethod = .sizeOne },
@@ -160,5 +160,104 @@ extension View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+// MARK: - PopOver workaround
+// This code was taken from https://stackoverflow.com/questions/56910941/present-actionsheet-in-swiftui-on-ipad
+// to present a pop over sheet from an iPad because at this time SwiftUI doesn't seem to support
+// showing ActionSheets in iPads.
+public extension View {
+    /// Creates an `ActionSheet` on an iPhone or the equivalent `popover` on an iPad, in order to work around `.actionSheet` crashing on iPad (`FB7397761`).
+    ///
+    /// - Parameters:
+    ///     - isPresented: A `Binding` to whether the action sheet should be shown.
+    ///     - content: A closure returning the `PopSheet` to present.
+    func popSheet(isPresented: Binding<Bool>, arrowEdge: Edge = .bottom, content: @escaping () -> PopSheet) -> some View {
+        Group {
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                popover(isPresented: isPresented, attachmentAnchor: .rect(.bounds), arrowEdge: arrowEdge, content: { content().popover(isPresented: isPresented) })
+            } else {
+                actionSheet(isPresented: isPresented, content: { content().actionSheet() })
+            }
+        }
+    }
+}
+
+/// A `Popover` on iPad and an `ActionSheet` on iPhone.
+public struct PopSheet {
+    let title: Text
+    let message: Text?
+    let buttons: [PopSheet.Button]
+
+    /// Creates an action sheet with the provided buttons.
+    public init(title: Text, message: Text? = nil, buttons: [PopSheet.Button] = [.cancel()]) {
+        self.title = title
+        self.message = message
+        self.buttons = buttons
+    }
+
+    /// Creates an `ActionSheet` for use on an iPhone device
+    func actionSheet() -> ActionSheet {
+        ActionSheet(title: title, message: message, buttons: buttons.map({ popButton in
+            // convert from PopSheet.Button to ActionSheet.Button (i.e., Alert.Button)
+            switch popButton.kind {
+            case .default: return .default(popButton.label, action: popButton.action)
+            case .cancel: return .cancel(popButton.label, action: popButton.action)
+            case .destructive: return .destructive(popButton.label, action: popButton.action)
+            }
+        }))
+    }
+
+    /// Creates a `.popover` for use on an iPad device
+    func popover(isPresented: Binding<Bool>) -> some View {
+        VStack {
+            ForEach(Array(buttons.enumerated()), id: \.offset) { (offset, button) in
+                Group {
+                    SwiftUI.Button(action: {
+                        // hide the popover whenever an action is performed
+                        isPresented.wrappedValue = false
+                        // another bug: if the action shows a sheet or popover, it will fail unless this one has already been dismissed
+                        DispatchQueue.main.async {
+                            button.action?()
+                        }
+                    }, label: {
+                        button.label.font(.title)
+                    })
+                    Divider()
+                }
+            }
+        }
+    }
+
+    /// A button representing an operation of an action sheet or popover presentation.
+    ///
+    /// Basically duplicates `ActionSheet.Button` (i.e., `Alert.Button`).
+    public struct Button {
+        let kind: Kind
+        let label: Text
+        let action: (() -> Void)?
+        enum Kind { case `default`, cancel, destructive }
+
+        /// Creates a `Button` with the default style.
+        public static func `default`(_ label: Text, action: (() -> Void)? = {}) -> Self {
+            Self(kind: .default, label: label, action: action)
+        }
+
+        /// Creates a `Button` that indicates cancellation of some operation.
+        public static func cancel(_ label: Text, action: (() -> Void)? = {}) -> Self {
+            Self(kind: .cancel, label: label, action: action)
+        }
+
+        /// Creates an `Alert.Button` that indicates cancellation of some operation.
+        public static func cancel(_ action: (() -> Void)? = {}) -> Self {
+            Self(kind: .cancel, label: Text("Cancel"), action: action)
+        }
+
+        /// Creates an `Alert.Button` with a style indicating destruction of some data.
+        public static func destructive(_ label: Text, action: (() -> Void)? = {}) -> Self {
+            Self(kind: .destructive, label: label, action: action)
+        }
     }
 }
