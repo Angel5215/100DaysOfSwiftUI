fb24163238f4439c17dff910f045830d1e1b4928
Ángel Vázquez <angel_betta@outlook.com>
2024-06-01 23:45:56 -0600

    Add initial files for WeSplit project

---
+++ Days/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,15 @@
+//
+// WeSplitApp.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
