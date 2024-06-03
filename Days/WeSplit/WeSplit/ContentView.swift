3e5d34183378626634cbf731e81e35fb661b9f3e
Ángel Vázquez <angel_betta@outlook.com>
2024-06-03 13:04:21 -0600

    DAY-016: Show amount in `TextField` with currency format

--- Days/WeSplit/WeSplit/ContentView.swift
+++ Days/WeSplit/WeSplit/ContentView.swift
@@ -6,14 +6,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField(
+                    "Amount",
+                    value: $checkAmount,
+                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                )
+            }
+
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
         }
-        .padding()
     }
 }
 
