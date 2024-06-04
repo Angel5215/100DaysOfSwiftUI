007e767d36835aef247c5b2f6f0cd9c3e0bb7f21
Ángel Vázquez <angel_betta@outlook.com>
2024-06-03 20:53:38 -0600

    DAY-018: [CHALLENGE] Add header to third section saying "Amount per person"

--- Days/WeSplit/WeSplit/ContentView.swift
+++ Days/WeSplit/WeSplit/ContentView.swift
@@ -53,7 +53,7 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
 
-                Section {
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
