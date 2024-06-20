882b9aa918cca2778395dfd3cb98f99166366508
Ángel Vázquez <angel_betta@outlook.com>
2024-06-20 01:00:33 -0600

    DAY-020: Add initial files for project GuessTheFlag

---
+++ Days/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,15 @@
+//
+// GuessTheFlagApp.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
