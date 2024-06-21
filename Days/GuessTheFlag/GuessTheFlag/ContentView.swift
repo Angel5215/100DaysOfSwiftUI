772b2dda4aa9a63dd851beec868488318dc5b8cc
Ángel Vázquez <angel_betta@outlook.com>
2024-06-21 11:30:02 -0600

    DAY-021: Style flags and text

--- Days/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Days/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -26,14 +26,20 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
+            LinearGradient(
+                colors: [.blue, .black],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
 
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
+                        .font(.subheadline.weight(.heavy))
                         .foregroundStyle(.white)
                     Text(countries[correctAnswer])
+                        .font(.largeTitle.weight(.semibold))
                         .foregroundStyle(.white)
                 }
 
@@ -42,6 +48,8 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
             }
