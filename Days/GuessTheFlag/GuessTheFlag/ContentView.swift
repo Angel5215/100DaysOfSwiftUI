a19794c97b2fc4c2f04aa03621d4a3c8cf1016d5
Ángel Vázquez <angel_betta@outlook.com>
2024-06-21 11:18:56 -0600

    DAY-021: Draft for the UI

--- Days/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Days/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -6,14 +6,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Nigeria",
+        "Poland",
+        "Spain",
+        "UK",
+        "Ukraine",
+        "US",
+    ]
+    var correctAnswer = Int.random(in: 0 ... 2)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+
+                ForEach(0 ..< 3) { number in
+                    Button {
+                        // flag was tapped
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
