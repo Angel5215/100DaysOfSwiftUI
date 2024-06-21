7eec8ed96710d014196fd55acf5320a7a346dbca
Ángel Vázquez <angel_betta@outlook.com>
2024-06-21 11:26:06 -0600

    DAY-021: Show alert on flag tap

--- Days/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Days/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -6,7 +6,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = [
+    @State private var countries = [
         "Estonia",
         "France",
         "Germany",
@@ -18,8 +18,11 @@ struct ContentView: View {
         "UK",
         "Ukraine",
         "US",
-    ]
-    var correctAnswer = Int.random(in: 0 ... 2)
+    ].shuffled()
+
+    @State private var correctAnswer = Int.random(in: 0 ... 2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
 
     var body: some View {
         ZStack {
@@ -36,13 +39,33 @@ struct ContentView: View {
 
                 ForEach(0 ..< 3) { number in
                     Button {
-                        // flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                     }
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0 ... 2)
     }
 }
 
