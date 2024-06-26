3349959ec89193817e9d85353031d0b17565c157
Ángel Vázquez <angel_betta@outlook.com>
2024-06-26 00:58:06 -0600

    DAY-023: Move `GridStack` to its own file

--- Days/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Days/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -37,24 +37,6 @@ struct ContentView: View {
     }
 }
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    @ViewBuilder let content: (Int, Int) -> Content
-
-    var body: some View {
-        VStack {
-            ForEach(0 ..< rows, id: \.self) { row in
-                HStack {
-                    ForEach(0 ..< columns, id: \.self) { column in
-                        content(row, column)
-                    }
-                }
-            }
-        }
-    }
-}
-
 #Preview {
     ContentView()
 }
