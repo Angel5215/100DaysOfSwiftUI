8e84cf926360e8e23c8a7b2261743c0316fbe744
Ángel Vázquez <angel_betta@outlook.com>
2024-06-26 22:29:20 -0600

    DAY-025: (CHALLENGE) Add new project files for challenge 2

---
+++ Challenges/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,15 @@
+//
+// RockPaperScissorsApp.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
