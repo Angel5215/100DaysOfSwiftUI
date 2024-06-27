4a385b0ada66c3420ddb0a8b23c5c730630bbae4
Ángel Vázquez <angel_betta@outlook.com>
2024-06-26 22:54:03 -0600

    DAY-025: (CHALLENGE) Add main logic for game

--- Challenges/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ Challenges/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -6,14 +6,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let moves = ["Rock", "Paper", "Scissors"]
+    @State private var currentChoice = Int.random(in: 0 ..< 2)
+    @State private var shouldPlayerWin = Bool.random()
+    @State private var score = 0
+
+    private var shouldPlayerWinText: String {
+        if shouldPlayerWin {
+            "win"
+        } else {
+            "lose"
+        }
+    }
+
+    private var currentChoiceText: String {
+        moves[currentChoice]
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Score: \(score)")
+
+            Text("You should \(shouldPlayerWinText) against \(currentChoiceText)")
+
+            VStack {
+                Text("Select your move:")
+
+                Button("Rock") {
+                    updateScore(userMove: "Rock")
+                }
+                Button("Paper") {
+                    updateScore(userMove: "Paper")
+                }
+                Button("Scissors") {
+                    updateScore(userMove: "Scissors")
+                }
+            }
         }
-        .padding()
+    }
+
+    private func result(for userMove: String, against systemMove: String, shouldWin: Bool) -> Bool {
+        switch (shouldWin, userMove, systemMove) {
+        case (true, "Rock", "Scissors"), (true, "Paper", "Rock"), (true, "Scissors", "Paper"):
+            true
+        case (false, "Rock", "Paper"), (false, "Paper", "Scissors"), (false, "Scissors", "Rock"):
+            true
+        default:
+            false
+        }
+    }
+
+    private func updateScore(userMove: String) {
+        let systemMove = moves[currentChoice]
+        if result(for: userMove, against: systemMove, shouldWin: shouldPlayerWin) {
+            score += 1
+        } else {
+            score -= 1
+        }
+
+        shuffleMoves()
+    }
+
+    private func shuffleMoves() {
+        currentChoice = Int.random(in: 0 ..< 2)
+        shouldPlayerWin = Bool.random()
     }
 }
 
