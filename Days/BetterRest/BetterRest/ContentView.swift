4ca1cdc1cb16a2e5ba307b2e7bb1985dc47ae2f3
Ángel Vázquez <angel_betta@outlook.com>
2024-06-29 01:05:59 -0600

    DAY-027: Build a basic layout for app

--- Days/BetterRest/BetterRest/ContentView.swift
+++ Days/BetterRest/BetterRest/ContentView.swift
@@ -6,15 +6,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+
+                DatePicker(
+                    "Please enter a time",
+                    selection: $wakeUp,
+                    displayedComponents: .hourAndMinute
+                )
+                .labelsHidden()
+
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper(
+                    "\(sleepAmount.formatted()) hours",
+                    value: $sleepAmount,
+                    in: 4 ... 12,
+                    step: 0.25
+                )
+
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1 ... 20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
-        .padding()
     }
+
+    func calculateBedtime() {}
 }
 
 #Preview {
