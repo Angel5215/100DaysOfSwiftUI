b239e2a7fc33c7b395a68467d40be77ee77fe0ab
Ángel Vázquez <angel_betta@outlook.com>
2024-07-09 15:04:00 -0600

    DAY-030: Run list of word when view appears

--- Days/WordScramble/WordScramble/ContentView.swift
+++ Days/WordScramble/WordScramble/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
 
@@ -42,6 +43,18 @@ struct ContentView: View {
         }
 
         newWord = ""
+    }
+
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
