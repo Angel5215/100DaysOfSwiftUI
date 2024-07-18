87a481dda9ff53d797f8ffa464598ab24e403a1f
Ángel Vázquez <angel_betta@outlook.com>
2024-07-18 10:39:47 -0600

    DAY-032: Add initial files for project 6

---
+++ Days/Animations/Animations/ContentView.swift
@@ -0,0 +1,22 @@
+//
+// ContentView.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
