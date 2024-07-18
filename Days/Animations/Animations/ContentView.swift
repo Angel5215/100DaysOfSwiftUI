b11733b25e85fd71cba8fcbf459e00fd92883935
Ángel Vázquez <angel_betta@outlook.com>
2024-07-18 10:47:40 -0600

    DAY-032: Example of Implicit animations

--- Days/Animations/Animations/ContentView.swift
+++ Days/Animations/Animations/ContentView.swift
@@ -8,12 +8,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ImplicitAnimationsView()
         }
-        .padding()
     }
 }
 
