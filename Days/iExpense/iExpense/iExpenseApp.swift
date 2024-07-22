83ca2ac5c83f98f9e151c54f7a393e5a806a8160
Ángel Vázquez <angel_betta@outlook.com>
2024-07-22 07:29:32 -0600

    Initial files for project 7

---
+++ Days/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,15 @@
+//
+// iExpenseApp.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
