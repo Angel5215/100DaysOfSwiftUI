c3e4bc7d103a729a10d24ef203fad832c34948fd
Ángel Vázquez <angel_betta@outlook.com>
2024-07-23 01:18:48 -0600

    Make initial draft of `AddView`

---
+++ Days/iExpense/iExpense/AddView.swift
@@ -0,0 +1,36 @@
+//
+// AddView.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView()
+}
