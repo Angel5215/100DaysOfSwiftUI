fac3f51eb6a65c106c569fe3738d85c608feddb8
Ángel Vázquez <angel_betta@outlook.com>
2024-07-23 00:41:48 -0600

    Add `@Observable` `Expenses` model

--- Days/iExpense/iExpense/ExpenseItem.swift
+++ Days/iExpense/iExpense/ExpenseItem.swift
@@ -3,8 +3,15 @@
 // Last modified by 'Ángel Vázquez' in 2024.
 //
 
+import Observation
+
 struct ExpenseItem {
     let name: String
     let type: String
     let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
 }
