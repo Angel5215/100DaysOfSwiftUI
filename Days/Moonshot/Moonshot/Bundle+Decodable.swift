c33fc38a36e70ef7f4490ed74260c4063b0c5699
Ángel Vázquez <angel_betta@outlook.com>
2024-07-27 09:22:00 -0600

    DAY-040: Improve error messages while decoding local file from Bundle

---
+++ Days/Moonshot/Moonshot/Bundle+Decodable.swift
@@ -0,0 +1,34 @@
+//
+// Bundle+Decodable.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        do {
+            return try decoder.decode([String: Astronaut].self, from: data)
+        } catch let DecodingError.keyNotFound(key, context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' – \(context.debugDescription)")
+        } catch let DecodingError.typeMismatch(_, context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+        } catch let DecodingError.valueNotFound(type, context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+    }
+}
--- Days/Moonshot/Moonshot/ContentView.swift
+++ Days/Moonshot/Moonshot/ContentView.swift
@@ -6,12 +6,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(astronauts.count))
         }
         .padding()
     }
