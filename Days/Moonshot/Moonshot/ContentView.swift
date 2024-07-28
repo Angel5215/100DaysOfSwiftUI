690ff274040ee56a647e654afe88beee37e6f2b0
Ángel Vázquez <angel_betta@outlook.com>
2024-07-28 01:25:42 -0600

    DAY-040: First pass to main view

--- Days/Moonshot/Moonshot/ContentView.swift
+++ Days/Moonshot/Moonshot/ContentView.swift
@@ -9,11 +9,38 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
 
+    let columns = [
+        GridItem(.adaptive(minimum: 150)),
+    ]
+
     var body: some View {
-        VStack {
-            Text(String(astronauts.count))
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
         }
-        .padding()
     }
 }
 
