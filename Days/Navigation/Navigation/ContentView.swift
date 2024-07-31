956381ad5234a7d921c1645d119140e228354355
Ángel Vázquez <angel_betta@outlook.com>
2024-07-31 15:00:13 -0600

    DAY-044: Showcase programatic and user navigation

--- Days/Navigation/Navigation/ContentView.swift
+++ Days/Navigation/Navigation/ContentView.swift
@@ -6,14 +6,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var path = NavigationPath()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List {
+                ForEach(0 ..< 5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+
+                ForEach(0 ..< 5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+            .toolbar {
+                Button("Push 556") {
+                    path.append(556)
+                }
+
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
         }
-        .padding()
     }
 }
 
