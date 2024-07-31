5311dec8d8c17e19dcd072d903fa0dc5f989d155
Ángel Vázquez <angel_betta@outlook.com>
2024-07-30 21:01:30 -0600

    DAY-043: Add initial files for project 9

---
+++ Days/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,15 @@
+//
+// NavigationApp.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
