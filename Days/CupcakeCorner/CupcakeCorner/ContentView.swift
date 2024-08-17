de1a8d7462f3e5655bdc4db93ca2e62b2c931a84
Ángel Vázquez <angel_betta@outlook.com>
2024-08-17 11:45:53 -0600

    DAY-049: Load list of music items async

--- Days/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Days/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -5,15 +5,57 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else if phase.error != nil {
+                        Text("There was an error loading the image")
+                    } else {
+                        ProgressView()
+                    }
+                }
+                .frame(width: 200, height: 200)
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            return print("Invalid URL")
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
