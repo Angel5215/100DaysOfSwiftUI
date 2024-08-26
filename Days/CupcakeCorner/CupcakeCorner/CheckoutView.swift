95ede419a78a296c1a3390c913337851c07f35b6
Ángel Vázquez <angel_betta@outlook.com>
2024-08-26 14:12:11 -0600

    DAY-051: Add `CheckoutView`

---
+++ Days/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+// CheckoutView.swift
+// Last modified by 'Ángel Vázquez' in 2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
